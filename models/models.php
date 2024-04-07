c13f8114e76eddf186b9dcfc3c5b04512aaa1fd3
rekaananda1019 <146187561+rekaananda1019@users.noreply.github.com>
2024-04-07 19:54:57 +0700

    Add files via upload

---
+++ models/models.php
@@ -0,0 +1,26 @@
+<?php
+
+Class Dashboard{
+    public $id_contact;
+    public $id_akun;
+    public $owner;
+    public $no_hp;
+    public $created_at;
+    public $updated_at;
+}
+
+Class Contact{
+    static function select (){
+
+    }
+    static function insert (){
+        
+    }
+    static function update (){
+        
+    }
+    static function delete (){
+        
+    }
+}
+
